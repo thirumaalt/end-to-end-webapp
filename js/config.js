365b80906b9d1b90d34102d27b7e6bfaf4eb6d9a
Thirumaal T <67221923+thirumaalt@users.noreply.github.com>
2025-04-07 15:35:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_wc6JDr2Ds', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6kni6vkn368fvai3u8f3igskhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1fl550qvigtlvj66ndc1rpccs3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
