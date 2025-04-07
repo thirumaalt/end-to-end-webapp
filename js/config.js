b9986969c415e11e36650dc62127a1d3faa88792
Thirumaal T <67221923+thirumaalt@users.noreply.github.com>
2025-04-07 15:17:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_wc6JDr2Ds', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6kni6vkn368fvai3u8f3igskhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
